f05ddc8ded61647298d32edfeb51a937c4280418
Pitch-dev <pitch.dev.85@gmail.com>
2025-03-17 19:46:24 +0100

    Enable modification

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='akai_pro_py',
-    version='0.3.2',
+    version='0.3.2-0',
     description='A simple library for interfacing with the Akai Professional series of MIDI controllers',
-    url='https://github.com/dhoessl/apc-mini-py',
-    author='Secret and Lewis L. Foster, Dominic Hößl',
-    author_email='Secret <unknown>, Lewis L. Foster <lewis@sniff122.tech>, Dominic Hößl <dominichoessl@gmail.com>',
+    url='https://github.com/pitch-dev/apc-mini-py',
+    author='Secret and Lewis L. Foster, Dominic Hößl, Pitch-dev',
+    author_email='Secret <unknown>, Lewis L. Foster <lewis@sniff122.tech>, Dominic Hößl <dominichoessl@gmail.com>, Pitch-dev',
     license='',
     packages=['akai_pro_py'],
     install_requires=['mido',
